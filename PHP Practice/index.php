059acc7201e26c2764c7be5e5af8b838a33f2535
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-12-26 16:33:08 +0500

    Functions

--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -22,16 +22,59 @@
         // echo ++$number1;
         // echo "<br>";
 
-        $age = 60;
-        echo "<br>";
-        if($age >= 60){
-            echo "You are not eligible to Vote";
-        }else if($age >= 18){
-            echo "You are eligible to Vote";
-        }else{
-            echo "You are not eligible to Vote";
-        }
+        // $age = 60;
+        // echo "<br>";
+        // if($age >= 60){
+        //     echo "You are not eligible to Vote";
+        // }else if($age >= 18){
+        //     echo "You are eligible to Vote";
+        // }else{
+        //     echo "You are not eligible to Vote";
+        // }
+
+        $languages = array("Python", "Php", "Java", "C++", "C#","Javascript","Ruby");
+        // echo "<br>";
+        // echo count($languages);
+
+        $name = "Suleman";
+        // $age = 19;
+        $PI = 3.14;
+
+        // echo "<br>";
+        // echo "$name is  $age years old and the value of pi is $PI";
+        
+        // do{
+            //     echo "<br> The value is $val";
+            //     $val++;
+            // }while($val <= 10);
+            
+    //     $val =0;
+
+    //  while($val < count($languages)){
+    //     echo "<br> The value is $languages[$val]";
+    //     $val++;
+    //  }
+    echo "<br>";
+
+    print_r($languages);
+
+    foreach($languages as $value){
+        echo "<br> The value is $value";
+    }
+    // String Functions in PHP
+    echo "<br>";
+    echo var_dump($name);
+    echo "<br>";
+    echo strpos($name, 'm');
+    echo "<br>";
+    echo gettype($PI);
+    echo "<br>";
+    echo gettype($PI);
+    echo "<br>";
+    echo gettype($PI);
     ?>
+
+    
     <br>
     <button class="btn btn-success">Click me</button>
 
@@ -41,8 +84,9 @@
         btn.addEventListener('click', function(){
             body.style.backgroundColor = 'black';
             body.style.color = 'white';
+
         });
-    </script>
+        </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
