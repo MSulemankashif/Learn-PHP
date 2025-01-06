4715ff271f38ff2ad1ad9be479ea2e51d31284bc
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-01-06 13:44:01 +0500

    update

---
+++ PHP Practice/dump.php
@@ -0,0 +1,67 @@
+<?
+echo "Hello World from PHP";
+
+        // $number1 = 10;
+        // $number2 = 21;
+        //     echo "<br>";
+        // echo $number1 + $number2;
+        // echo ++$number1;
+        // echo "<br>";
+
+        // $age = 60;
+        // echo "<br>";
+        // if($age >= 60){
+        //     echo "You are not eligible to Vote";
+        // }else if($age >= 18){
+        //     echo "You are eligible to Vote";
+        // }else{
+        //     echo "You are not eligible to Vote";
+        // }
+
+        $languages = array("Python", "Php", "Java", "C++", "C#","Javascript","Ruby");
+        // echo "<br>";
+        // echo count($languages);
+
+        $name = "Suleman";
+        // $age = 19;
+        $PI = 3.14;
+
+        // echo "<br>";
+        // echo "$name is  $age years old and the value of pi is $PI";
+        
+        // do{
+            //     echo "<br> The value is $val";
+            //     $val++;
+            // }while($val <= 10);
+            
+    //     $val =0;
+
+    //  while($val < count($languages)){
+    //     echo "<br> The value is $languages[$val]";
+    //     $val++;
+    //  }
+    echo "<br>";
+
+    print_r($languages);
+
+    foreach($languages as $value){
+        echo "<br> The value is $value";
+    }
+    // String Functions in PHP
+    echo "<br>";
+    echo var_dump($name);
+    echo "<br>";
+    echo strpos($name, 'm');
+    echo "<br>";
+    echo gettype($PI);
+    echo "<br>";
+    echo strlen($PI);
+    echo "<br>";
+    echo gettype($PI);
+    echo "<br>";
+
+    $str = "This is a String";
+
+    $lenn = strlen($str);
+    echo "The length of the string is $lenn <br>";
+    ?>
