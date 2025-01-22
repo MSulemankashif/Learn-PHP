64678bc89a3eddde0569d8c3f69a66c7df05c0c3
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-01-22 12:13:14 +0500

    update

--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -1,18 +1,23 @@
 <?php 
 
-$favoriteColor = "blur";
+//Switch case statement
+// $favoriteColor = "blur";
 
-switch($favoriteColor){
-    case "blue":
-        echo "Your favorite color is blue";
-        break;
-    case "red":
-        echo "Your favorite color is red";
-        break;
-    case "orange":
-        echo "Your favorite color is orange";
-        break;
-    default:
-        echo "You like all colors";
-        break;
+// switch($favoriteColor){
+//     case "blue":
+//         echo "Your favorite color is blue";
+//         break;
+//     case "red":
+//         echo "Your favorite color is red";
+//         break;
+//     case "orange":
+//         echo "Your favorite color is orange";
+//         break;
+//     default:
+//         echo "You like all colors";
+//         break;
+// };
+
+for($i = 0;$i <=10;$i++){
+    echo "The Value is $i". "<br>";
 };
