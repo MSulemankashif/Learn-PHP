9be9024a1829da5ada2caf39976edcc1e9bc47b5
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-01-26 13:27:23 +0500

    Making functions /go to statements

--- PHP Practice/dump.php
+++ PHP Practice/dump.php
@@ -101,3 +101,36 @@ echo "Hello World from PHP";
         print_r($Tiger);
         echo "<br>";
         print_r($goat);
+
+        //Switch case statement
+// $favoriteColor = "blur";
+
+// switch($favoriteColor){
+//     case "blue":
+//         echo "Your favorite color is blue";
+//         break;
+//     case "red":
+//         echo "Your favorite color is red";
+//         break;
+//     case "orange":
+//         echo "Your favorite color is orange";
+//         break;
+//     default:
+//         echo "You like all colors";
+//         break;
+// };
+
+$y =20;
+echo 'Before Execution ';
+if($y == 20){
+    goto end;
+}
+echo "Hello world";
+
+end: echo"<br> after execution";
+
+function printName($name,$color){
+    echo "<h1 style='color:$color'>Your name is $name</h1>";
+};
+
+printName("Suleman","green");
--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -1,28 +1,5 @@
 <?php 
 
-//Switch case statement
-// $favoriteColor = "blur";
+//Making functions
 
-// switch($favoriteColor){
-//     case "blue":
-//         echo "Your favorite color is blue";
-//         break;
-//     case "red":
-//         echo "Your favorite color is red";
-//         break;
-//     case "orange":
-//         echo "Your favorite color is orange";
-//         break;
-//     default:
-//         echo "You like all colors";
-//         break;
-// };
 
-$y =20;
-echo 'Before Execution ';
-if($y == 20){
-    goto end;
-}
-echo "Hello world";
-
-end: echo"<br> after execution";
