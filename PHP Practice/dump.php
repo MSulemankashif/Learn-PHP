ef5fd37a2e26545bae2f3be14143575aaf4d4187
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-01-30 17:14:23 +0500

    print data in table using multidimentional array

--- PHP Practice/dump.php
+++ PHP Practice/dump.php
@@ -222,3 +222,16 @@ for($i=0; $i<count($users);$i++){
         echo $users[$i] [$j]. "<br>";
     };
 };
+
+
+$users = [
+    [1,"Suleman","Karachi"],
+    [2,"Usman","Lahore"],
+    [3,"Ali",'Islamabad']    
+];
+
+for($i=0; $i<count($users);$i++){
+    for($j=0; $j<count($users[$i]);$j++){
+        echo $users[$i][$j]. " <br>";
+    };
+};
--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -1,13 +1,33 @@
 <?php 
 
-$users = [
+$students = [
     [1,"Suleman","Karachi"],
-    [2,"Usman","Lahore"],
-    [3,"Ali",'Islamabad']    
+    [2,"Ali","Lahore"],
+    [3,"Ahmed","Islamabad"],
+    [4,"Kamran","Quetta"],
+    [5,"Noman","Peshawar"],
+    [6,"Talha","Multan"],
+    [7,"Bilal","Faisalabad"],
+    [8,"Zain","Hyderabad"],
+    [9,"Ahsan","Sukkur"],
+    [10,"Usman","Larkana"]
 ];
 
-for($i=0; $i<count($users);$i++){
-    for($j=0; $j<count($users[$i]);$j++){
-        echo $users[$i][$j]. " <br>";
+echo "<table border=1>";
+    echo "<h1>Students Data</h1>";
+    echo "<tr>
+        <td style='font-weight:bold';>ID</td>
+        <td style='font-weight:bold';>Name</td>
+        <td style='font-weight:bold';>City</td>
+          </tr>";
+    for($i=0; $i<count($students); $i++){
+        echo "<tr>";
+            for($j=0; $j<count($students[$i]); $j++){
+                echo "<td>";
+                echo $students[$i][$j];
+                echo "</td>";
+            };
+        echo "</tr>";
     };
-};
+echo "</table>";
+
