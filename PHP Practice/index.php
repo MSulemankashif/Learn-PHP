411b96e0839397437d85197cacab6bbb4a4ae9e2
Shah Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-01-31 20:50:12 +0500

    update

--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -1,23 +1,10 @@
 <?php
-$users = [
-    [1,"Suleman","Karachi"],
-    [2,"Usman","Karachi"],
-    [3,"Ali","Islamabad"],
-    [4,"Waleed","lahore"]
-];
+$students = array(
+    "name"=>"Suleman",
+    "age"=>19,
+    "city"=>"karachi"
+);
 
-echo "<table border=1>";
-    echo "<tr>";
-    echo "<td style='font-weight:bold';>S.NO</td>";
-    echo "<td style='font-weight:bold';>Name</td>";
-    echo "<td style='font-weight:bold';>City</td>";
-    echo "</tr>";
-    for($i=0;$i<count($users);$i++){
-    echo "<tr>";
-        for($j=0;$j<count($users[$i]);$j++){
-            echo "<td>";
-            echo $users[$i][$j] ."<br>";
-            echo "</td>";
-        }
-    echo "</tr>";
-    };
+foreach ($students as $key=>$data){
+    echo "the key is $key and the value is $data ". "<br>";
+}
