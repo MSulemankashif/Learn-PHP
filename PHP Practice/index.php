1f325d377e359410c11814873b3df51605d235c4
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-08 12:39:06 +0500

    Update

--- PHP Practice/index.php
+++ PHP Practice/index.php
@@ -2,33 +2,24 @@
 
 $students = [
     [1,"Suleman","Karachi"],
-    [2,"Ali","Lahore"],
-    [3,"Ahmed","Islamabad"],
-    [4,"Kamran","Quetta"],
-    [5,"Noman","Peshawar"],
-    [6,"Talha","Multan"],
-    [7,"Bilal","Faisalabad"],
-    [8,"Zain","Hyderabad"],
-    [9,"Ahsan","Sukkur"],
-    [10,"Usman","Larkana"]
+    [2,"Usman","Lahore"],
+    [3,"Ali","Islamabad"],
+    [4,"Ahmed","Quetta"],
+    [5,"Kamran","Peshawar"],
+    [6,"Noman","Multan"],
+    [7,"Talha","Faisalabad"],
+    [8,"Bilal","Sialkot"],
+    [9,"Zain","Gujranwala"],
+    [10,"Ahsan","Hyderabad"]
 ];
 
 echo "<table border=1>";
     echo "<h1>Students Data</h1>";
-    echo "<tr>
-        <td style='font-weight:bold';>ID</td>
-        <td style='font-weight:bold';>Name</td>
-        <td style='font-weight:bold';>City</td>
-          </tr>";
-    for($i=0; $i<count($students); $i++){
+    for($i=0;$i<count($students);$i++){
         echo "<tr>";
-            for($j=0; $j<count($students[$i]); $j++){
-                echo "<td>";
-                echo $students[$i][$j];
-                echo "</td>";
-            };
+        for($j=0;$j<count($students[$i]);$j++){
+            echo "<td>" . $students[$i][$j] . "</td>";
+        }
         echo "</tr>";
-    };
-echo "</table>";
-
-// echo "Hello World";
+    }
+    echo "</table>";
