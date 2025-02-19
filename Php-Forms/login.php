ed1ec1fafbb4a818d60b53abac958dee74b8b9ce
Shah Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-19 20:25:07 +0500

    Sending Data With Post Request

--- Php-Forms/login.php
+++ Php-Forms/login.php
@@ -4,8 +4,12 @@ if($_GET){
     echo "Your name is ". $_GET['username'];
     echo "<br>";
     echo "Your password is ". $_GET['password'];
-} else{
-
-    echo "hello world";
+} else if($_POST){
+    echo "Your name is " .$_POST['username'];
+    echo "<br>";
+    echo "Your password is " .$_POST['password'];
 }
+else{
+    echo "hello world";
+};
 
