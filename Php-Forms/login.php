3a31b25a338f356b1a7b8e35a8ff452703e9f213
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-21 15:30:16 +0500

    $_Request (Super Global Variable)

--- Php-Forms/login.php
+++ Php-Forms/login.php
@@ -1,15 +1,17 @@
 <?php
 
-if($_GET){
-    echo "Your name is ". $_GET['username'];
-    echo "<br>";
-    echo "Your password is ". $_GET['password'];
-} else if($_POST){
-    echo "Your name is " .$_POST['username'];
-    echo "<br>";
-    echo "Your password is " .$_POST['password'];
-}
-else{
-    echo "hello world";
-};
+// if($_GET){
+//     echo "Your name is ". $_GET['username'];
+//     echo "<br>";
+//     echo "Your password is ". $_GET['password'];
+// } else if($_POST){
+//     echo "Your name is " .$_POST['username'];
+//     echo "<br>";
+//     echo "Your password is " .$_POST['password'];
+// }
+// else{
+//     echo "hello world";
+// };
 
+
+print_r( $_POST); 
