72bbc688d6d96abd7a94a80afde057809be6ac57
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-22 14:39:57 +0500

    Practice File Upload

--- File_Upload/file.php
+++ File_Upload/file.php
@@ -2,12 +2,24 @@
 
 // print_r($_FILES);
 
+// if($_FILES['fileUpload']['name']){
+//     $path = $_FILES['fileUpload']['name'];
+//     $upload_path = './uploads/'.$path;
+//     if(move_uploaded_file($_FILES['fileUpload']['tmp_name'], $upload_path)){
+//         echo 'File Uploaded';
+//     }else{
+//         die ('File not uploaded');
+//     };
+// }
+
+// Practice
+// print_r($_FILES);
+
 if($_FILES['fileUpload']['name']){
-    $path = $_FILES['fileUpload']['name'];
-    $upload_path = './uploads/'.$path;
-    if(move_uploaded_file($_FILES['fileUpload']['tmp_name'], $upload_path)){
+    $path = './uploads/'.$_FILES['fileUpload']['name'];
+    if(move_uploaded_file($_FILES['fileUpload']['tmp_name'], $path)){
         echo 'File Uploaded';
-    }else{
-        die ('File not uploaded');
+    } else{
+        die('File not uploaded');
     };
 }
