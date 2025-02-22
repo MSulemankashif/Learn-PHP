51329015b6191d2c01895258f175c3b2c597a628
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-22 14:46:16 +0500

    Setting Cookie

--- Cookies/cookies.php
+++ Cookies/cookies.php
@@ -1,2 +1,13 @@
 <?php
 
+// Set cookie
+$name = "Suleman";
+setcookie("name", $name, time() + (86400 ));
+
+// print_r($_COOKIE);
+if(isset($_COOKIE['name'])){
+    echo "Cookie is set";
+}else{
+    echo "Cookie is not set";
+}
+
