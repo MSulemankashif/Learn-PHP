4357550186e3c0767b966da1f134ae8501a0e64c
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-02-23 20:30:59 +0500

    Update

--- Write-file/write-file.php
+++ Write-file/write-file.php
@@ -8,30 +8,44 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </head>
+<style>
+    input, textarea{
+        width: 40% !important;
+    }
+</style>
 <body>
 <div class="container-fluid">
+    <h1>Create a File</h1>
     <form action="" method="post">
-        <input type="text" name="filename" placeholder="Enter file name">
+        <input type="text" name="filename" placeholder="Enter file name" required class="form-control mt-4">
         <br>
         <br>
-        <textarea name="content"></textarea>
-        <br>
+        <textarea name="content" placeholder="Enter content here" class="form-control"></textarea>
         <br>
     <button class="btn btn-outline-success">Create file</button>
     </form>
     </div>
 
     <?php
-    if(isset($_REQUEST['filename']) ){
-        $filename = 'files/'.$_REQUEST['filename'];
+    // if(isset($_REQUEST['filename']) ){
+    //     $filename = 'files/'.$_REQUEST['filename'];
+    //     $content = $_REQUEST['content'];
+    //     $file = fopen($filename, "w") or die ("Unable to Create file");
+    //     fwrite($file, $content);
+    //     fclose($file);
+    //     echo "File created successfully";
+    // }else{
+    //     echo "Please enter file name and content";
+    // }
+
+    if(isset($_REQUEST['filename'])){
+        $filename = 'files/'. $_REQUEST['filename'];
         $content = $_REQUEST['content'];
-        $file = fopen($filename, "w") or die ("Unable to Create file");
+        $file = fopen($filename, "w") or die("unable to open file");
         fwrite($file, $content);
         fclose($file);
         echo "File created successfully";
-    }else{
-        echo "Please enter file name and content";
-    }
+    };
     ?>
 </body>
 </html>
