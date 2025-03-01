37ea8550d589b9a008bfdd243171d21a31881486
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-01 15:44:19 +0500

    Date & Time

--- Handle_with_json/date-time.php
+++ Handle_with_json/date-time.php
@@ -1,4 +1,9 @@
 <?php
 
 echo date_default_timezone_set('Asia/Karachi');
-echo date_default_timezone_get();
+
+echo date('D M Y');
+echo "<br>";
+echo date("h:i:s A");
+echo "<br>";
+echo date('S /o F Y h:i:s A');
