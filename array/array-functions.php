1b999160f93ef7e838408fbbae882eec47faa25a
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-03 15:48:09 +0500

    Update

---
+++ array/array-functions.php
@@ -0,0 +1,24 @@
+<?php
+
+$users = ["Suleman", "Ali", "Ahmed", "Usman", "Bilal", "Osama"];
+
+// echo count($users) . "<br>";
+// $count = 0;
+// foreach($users as $user){
+//     $count++;
+//     echo $count. " ";
+//     echo $user . "<br>";
+// }
+
+// if(is_array($users)){
+//     echo "This is an array";
+// }else{
+//     echo "This is not an array";
+// };
+
+// echo "This count of array is ".count($users) . "<br>";
+
+// unset($users[0]);
+array_push($users, "hassan");
+array_pop($users);
+print_r($users);
