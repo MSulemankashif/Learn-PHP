31487a96f13bebc65ed99324baac90856e31e2ee
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-06 16:01:32 +0500

    Update

--- html-forms/form.php
+++ html-forms/form.php
@@ -1,3 +1,20 @@
 <?php
 
+// echo "<pre>";
+// print_r($_POST);
+if(isset($_POST['name'])){
+    echo "Username is ". $_POST['name'];
+    echo "<br>";
+    echo "User email is " . $_POST["email"];
+    echo "<br>";
+    echo "User password is " . $_POST['password'];
+    echo "<br>";
+    echo "User skills is " . implode(", ", $_POST["skills"]);
+    echo "<br>";
+    if(!empty($_POST['bio'])){
+        echo "User bio is " . $_POST['bio'];
+    }else{
+        echo "User bio is not set";
+    };
+};
 
