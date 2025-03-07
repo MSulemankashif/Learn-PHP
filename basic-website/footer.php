527be408aa2753d2ce092fe3c448ae6328589ea3
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-07 19:54:52 +0500

    Basic Website

---
+++ basic-website/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <h2>This is a footer</h2>
+    <ul>
+        <li><a href="">About us</a></li>
+        <li><a href="">Contact us</a></li>
+        <li><a href="">FAQs</a></li>
+    </ul>
+</footer>
--- basic-website/index.php
+++ basic-website/index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap Site</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        include_once "header.php";
+    ?>
+    <main class="hero container-fluid">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum distinctio tempore modi excepturi vitae amet sint. Ducimus ut iusto faciliis omnis fuga, autem ex obcaecati harum repellendus est velit explicabo! Commodi quam deserunt expedita vero praesentium. Delectus nesciunt sed reprehenderit nisi voluptatem blanditiis ullam facere ex accusamus qui? Aperiam atque doloremque dolorem temporibus modi similique necessitatibus vel magnam facere quam? Et nesciunt, hic aperiam cum dolorem optio odit necessitatibus quis mollitia? Itaque vel iure similique? Molestiae aperiam, laboriosam distinctio dolorem ex, inventore deserunt voluptatibus quis laborum quia libero maxime! Aspernatur harum iure quisquam ipsum explicabo iusto reiciendis ipsam est consequuntur!</p>
+    </main>
+
+    <?php 
+        include_once "footer.php";
+    ?>
+</body>
+</html>
