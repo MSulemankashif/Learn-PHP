6ec32b9510aabf84cee28ed925de22818f69931c
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-18 12:33:17 +0500

    INSERT Update

--- Insert_Data/config.php
+++ Insert_Data/config.php
@@ -3,18 +3,10 @@ $host="localhost";
 $username="root";
 $password="";
 
-// try{
-//     $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     echo "Connected Successfully";
-    
-// }catch(PDOException $e){
-//     echo "Connection Failed: ".$e->getMessage();
-// }
 try{
     $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected Successfully";
+    echo "Connected Successfully <br>";
     // $sql = "Insert into students (Name, Course, Batch, City, Year) values ('John', 'B.Tech', '2020', 'Karachi', '2025')";
     // $conn->exec($sql);
     // echo "Data Inserted Successfully";
