2cfb0f223fd9f06bd1e530341e621f26dc7df4f1
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-03-29 15:20:58 +0500

    OOP

---
+++ OOPS/first.php
@@ -0,0 +1,21 @@
+<?php
+
+class MathOperations {
+    
+    public $val = 10;
+
+    // Method Declaration
+    function sum($a,$b){
+        echo $a+$b+$this->val;
+    }
+
+    function multi($a,$b){
+        echo $a*$b;
+    }
+}
+
+$sum = new MathOperations();
+    $sum->sum(20,20);
+    echo "<br>";
+$multiply = new MathOperations();
+    $multiply->multi(20,7);
